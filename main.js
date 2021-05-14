b57334a4d14d69fc7ec81f3b628d7fddbd35d558
Grace <gracemarsh27@gmail.com>
2021-05-14 22:35:17 +1000

    fix: import css through vite js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// import "./style.css";
 import "./libraries/css-toggle-switch-master/dist/toggle-switch.css";
+import "./normalize.css";
+import "./styles.css";
 
 // document.querySelector("#app").innerHTML = `
 //   <h1>Hello Vite!</h1>
