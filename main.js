b2edf5a52b058adf6a8c6b5ea4de3be6ef9d291e
Grace <gracemarsh27@gmail.com>
2021-05-14 12:41:07 +1000

    added toggle switch

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import "./style.css";
+import "./libraries/css-toggle-switch-master/dist/toggle-switch.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `;
 
 console.log("hello Grace you're doing great girl!");
